5c543634cf729e6d2e15c82b42bc854eddfc24f3
yacou keita <yacouba.keita@uvci.edu.ci>
2022-12-11 06:01:34 +0100

    chore(server):rendre l'url de swagger dinamic
    Ce commit permet d'ajouter le port de l'api a l'url de swagger de
    façon automatique.

--- server/app.js
+++ server/app.js
@@ -1,55 +1,57 @@
 // THIRD PARTY PACKAGE
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 
 // API ROUTE
-const clientRoute = require('./routes/clientRoutes');
-const providerRoute = require('./routes/providerRoutes');
+const clientRoute = require("./routes/clientRoutes");
+const providerRoute = require("./routes/providerRoutes");
 
-// Swagger 
-const swaggerUI = require('swagger-ui-express');
-const swaggerJsDoc = require('swagger-jsdoc');
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+
+const API_PORT = process.env.NODE_PORT;
 
 const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: 'Clients Manager API',
+      title: "Clients Manager API",
       version: "1.0.0",
-      description: "A simple express library API for manage clients and providers for clients"
+      description:
+        "A simple express library API for manage clients and providers for clients",
     },
     servers: [
       {
-        url: "http://localhost:3002"
-      }
-    ]
+        url: `http://localhost:${API_PORT}`,
+      },
+    ],
   },
-  apis: ['./routes/*.js']
+  apis: ["./routes/*.js"],
 };
 
 const specs = swaggerJsDoc(options);
 
-
 // EXPRESS APP
 const app = express();
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 // GLOBAL MIDDLEWARE
 app.use(cors());
-if (process.env.NODE_ENV === 'development') app.use(morgan('tiny'));
+if (process.env.NODE_ENV === "development") app.use(morgan("tiny"));
 app.use(express.json());
 
 // Redirect the home to clients routes
-app.get('/', (req, res) => res.redirect('/api/v1/clients'));
-app.use('/api/v1/clients', clientRoute);
-app.use('/api/v1/providers', providerRoute);
+app.get("/", (req, res) => res.redirect("/api/v1/clients"));
+app.use("/api/v1/clients", clientRoute);
+app.use("/api/v1/providers", providerRoute);
 
 // Handle error for all unknown routes
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
